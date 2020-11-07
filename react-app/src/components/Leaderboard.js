c647c63f22a4809ec5c088cd8a079b2e97dd02bc
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-11-06 19:52:14 -0800

    create API call for Leaderboard.js jk-1952

--- react-app/src/components/Leaderboard.js
+++ react-app/src/components/Leaderboard.js
@@ -1,27 +1,56 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import '../components/Leaderboard.css'
 
-const Leaderboard = () => (
+const Leaderboard = () => {
+const [loading, setLoading] = useState(true);
+const [score, setScore] = useState([]);
+const [firstName, setFirstName] = useState();
+const [lastName, setLastName] = useState();
+
+
+useEffect(() => {
+  setLoading(true)
+  async function getCategory(){
+  const response = await fetch('http://localhost:3000/api/scores')
+  const body = await response.json();
+  console.log(body);
+  // let categories = [];
+  // body.forEach(({category}) => {categories.push(category)});
+  // let singleCategories = [...new Set(categories)].map((category) => ({
+  //   value: category
+  // }));
+  console.log (body[1].firstName)
+  setScore(body[1].score)
+  setFirstName(body[1].firstName)
+  setLastName(body[1].lastName)
+  setLoading(false);
+  }
+getCategory();
+}, []);
+
+ return (
   <table className="table">
     <thead>
-      <tr>
+      {/* <tr>
         <th>First name</th>
         <th>Last name</th>
         <th>Score</th>
-      </tr>
+      </tr> */}
     </thead>
     <tbody>
       <tr>
-        <td>Temporary</td>
-        <td>Name</td>
-        <td>50</td>
+        <td>{firstName}</td>
+        <td>{lastName}</td>
+        <td>{score}</td>
       </tr>
-      <tr>
+      {/* <tr>
         <td>Name</td>
         <td>Placeholder</td>
         <td>20</td>
-      </tr>
+      </tr> */}
     </tbody>
   </table>
-)
+  );
+}
 
 export default Leaderboard
