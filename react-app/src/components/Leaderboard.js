deeafc19bba104d796a91b9ba000d151edafde6f
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-11-11 14:46:49 -0800

    Update Leaderboard.js

--- react-app/src/components/Leaderboard.js
+++ react-app/src/components/Leaderboard.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import EntryForm from "../components/EntryForm";
+// import EntryForm from "../components/EntryForm";
 import '../components/Leaderboard.css';
 
 const Leaderboard = () => {
 const [loading, setLoading] = useState(true);
 const [entry, setEntry] = useState({});
+const [user, setUser] = useState([]);
+
 
 
 useEffect(() => {
@@ -15,7 +17,7 @@ useEffect(() => {
       alert("The server in the warehouse server is down again. Please be try again later...did I stutter?!")
     } else {
     const body = await response.json();
-    let entries = Object.keys(body).map(function(key){
+    let entries = Object.keys(body).map((key) => {
       return body[key]}).sort((a,b) => b.score - a.score);
       setEntry(entries)
       setLoading(false);
@@ -23,6 +25,24 @@ useEffect(() => {
   }
 getScores();
 }, [])
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  e.target.reset();
+  fetch('http://localhost:3000/api/scores', {
+    method: 'POST',
+    body: JSON.stringify(user),
+  })
+  .then(res => res.json())
+  .then(json => setUser(json.user))
+  const newScore = {...user}
+  if(entry.reduce((prev, current) => (prev.score > current.score) ? prev : current).score < user.score){
+      console.log(`With a score of ${user.score}, ${user.firstName} is the new high score!`)
+  }
+  const newEntry = entry.slice();
+  newEntry.push(newScore)
+  setEntry(newEntry.sort((a,b) => b.score - a.score));
+}
 
 
 const sortByFirstName = () =>{
@@ -60,8 +80,12 @@ const sortByLowScore = () =>{
 
 const sortByHighScore = () =>{
   const entryByHighScore = entry.slice().sort((a,b) => b.score - a.score)
+  console.log(entryByHighScore)
   setEntry(entryByHighScore);
 };
+
+
+
 
 return (
   <div className="ui segment">
@@ -75,7 +99,7 @@ return (
       <table className="table">
       <tbody>
         {entry.map((e) => (
-          <tr key={e.id}>
+          <tr key={e.firstName}>
             <td >{e.firstName}</td>
             <td >{e.lastName}</td>
             <td >{e.score}</td>
@@ -109,7 +133,43 @@ return (
         <i className="sort amount up icon" />
       </div>
     </div>
-      <EntryForm setEntry={()=>setEntry} entry={entry}/>
+    <h2>Add an Entry</h2>
+    <form className="ui form" onSubmit={handleSubmit}>
+      <div className="field">
+        <label>First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          onChange={e => setUser({ ...user, firstName: e.target.value })}
+          required
+        />
+      </div>
+      <div className="field">
+      <label>Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          onChange={e => setUser({ ...user, lastName: e.target.value })}
+          required
+        />
+      </div>
+      <div className="field">
+        <label>Score</label>
+          <input
+            type="number"
+            name="score"
+            className="three wide field"
+            placeholder="Score"
+            onChange={e => setUser({ ...user, score: parseInt(e.target.value) })}
+            required
+            min = "0"
+            max="100"
+          />
+      </div>
+      <button className="ui button" type="submit">Submit</button>
+    </form >
     </div>
     }
   </div>
