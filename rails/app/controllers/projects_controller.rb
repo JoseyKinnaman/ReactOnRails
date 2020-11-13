a7f5d99fff29aeb660431d44fcde17f58e114589
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-11-13 15:34:20 -0800

    Add update and delete buttons for list. Create functionality to clear forms. Add Styling.

--- rails/app/controllers/projects_controller.rb
+++ rails/app/controllers/projects_controller.rb
@@ -2,4 +2,11 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.order(:name)
   end
+
+  def sort
+    params[:list].each_with_index do |id, index|
+      @project.lists.where(id: id).update_all(position: index + 1)
+    end
+    head 200
+  end
 end
